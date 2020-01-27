eaa1ed7a191fdf6baa3ab3636435853392eaa831
yoshimaru-ak <59866256+yoshimaru-ak@users.noreply.github.com>
2020-01-27 19:26:47 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){
-
   function buildHTML(message) {
-    if ( message.image ) {
+    if ( message.content && message.image ) {
       var html =
         `<div class="main-center__box" data-message-id=${message.id}>
           <div class="main-center__box__name">
@@ -20,7 +19,7 @@ $(function(){
           <img src=${message.image} >
         </div>`
       return html;
-    } else {
+    } else if (message.content) {
       var html =
         `<div class="main-center__box" data-message-id=${message.id}>
           <div class="main-center__box__name">
@@ -38,9 +37,21 @@ $(function(){
           </div>
         </div>`
       return html;
+    } else if (message.image) {
+      var html = `<div class="main-center__box" data-message-id=${message.id}>
+      <div class="main-center__box__name">
+          <div class="main-center__box__name__user">
+            ${message.user_name}
+          </div>
+          <div class="main-center__box__name__time">
+            ${message.created_at}
+          </div>
+        <img src=${message.image} >
+      </div>`
     };
   }
   $('.new_message').on('submit',function(e){
+    console.log("OK");
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -53,15 +64,43 @@ $(function(){
       contentType: false
     })
       .done(function(message){
+        console.log(message);
         var html = buildHTML(message);
         $('.main-center').append(html);
         $('.new_message')[0].reset();
-        $('.box').animate({'height' : '200px'});
+        // $('.box').animate({'height' : '200px'});
         $('.main-center').animate({ scrollTop: $('.main-center')[0].scrollHeight});
         $('.submit-btn').prop('disabled', false);
       })
       .fail(function() {
         alert("メッセージ送信に失敗しました");
     });
-  })
+  });
+    var reloadMessages = function() {
+      last_message_id = $('.main-center__box:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0){
+        var insertHTML = '';
+          $.each(messages, function(i,message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.main-center').append(insertHTML);
+          $('.main-center').animate({ scrollTop: $('.main-center')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
+      })
+      .fail(function(){
+        console.log('error');
+      })
+    }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
